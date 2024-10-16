df5bd7823347d40c893ab3a6941ef6ac511a6450
Norman Zhu <nzhu@ucsc.edu>
2024-10-16 18:36:28 +0000

    Step 1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Heeeeeeeeeelloooooooooooooo";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+// add an app title in a h1 element
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+
+app.append (title);
+
+// Add a canvas to the webpage of size 256x256 pixels.
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
